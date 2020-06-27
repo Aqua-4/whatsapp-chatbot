d706214df4746fb8d53581e1dcb290dbfd328387
aqua-4 <parashar.sangle@gramener.com>
2020-06-27 14:01:39 +0530

    ENH:

---
+++ chat_bot.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 01:58:38 2019
+
+@author: Parashar
+"""
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+#import logging
+from whatsapp_list import train_list
+
+
+'''
+This is an example showing how to train a chat bot using the
+ChatterBot Corpus of conversation dialog.
+'''
+
+# Enable info level logging
+# logging.basicConfig(level=logging.ERROR)
+
+chatbot = ChatBot('WhatsApp Bot')
+
+"""
+# Start by training our bot with the ChatterBot corpus data
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+
+# Train based on the english corpus
+trainer.train("chatterbot.corpus.english")
+
+# Train based on english greetings corpus
+trainer.train("chatterbot.corpus.english.greetings")
+
+# Train based on the english conversations corpus
+trainer.train("chatterbot.corpus.english.conversations")
+"""
+# trainer2 = ListTrainer(chatbot)
+# trainer2.train(train_list())
---
+++ whatsapp_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 21:32:55 2019
+
+@author: Parashar
+"""
+
+
+def train_list():
+
+    f_name = "WhatsApp Chat with Hrishikesh Dhatrak"
+    to = "Hrishikesh Dhatrak: "
+    me = "😈Parashar😇: "
+
+    op = []
+    f_ip = open("{}.txt".format(f_name), "r", encoding="utf8")
+    for line in f_ip:
+        res = line.split(to)[-1] if (to in line) else line.split(me)[-1]
+        op.append(res[:-1])
+    return op
